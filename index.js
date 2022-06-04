7fd75d0fc1f36c0e9d5cbc25d6c827848be6e02e
Ivan Tur <gajtep@gmail.com>
2022-06-04 12:19:50 +0300

    1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const message = "JavaScript is awesome!";
-alert(message);
+function formatMessage(message, maxLength) {
+  let result;
+  // Change code below this line
+  if (message.length <= maxLength) {
+    result = message;
+  } else {
+    result = message.slice(maxLength) + "...";
+  }
+  /// Change code above this line
+  return result;
+}
