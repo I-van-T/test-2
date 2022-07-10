61e5e923e95a79f1cae0e27757accac3bf9bea99
Ivan Tur <gajtep@gmail.com>
2022-07-10 12:46:12 +0300

    ;l

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let clients = ['Mango', 'Poly', 'Ajax', 'Kiwi'];
+function includes(array, value) {
+  // Change code below this line
+  for (let arr of array) {
+    if (arr === value) {
+      return true;
+    }
 
-console.table(clients);
+    if (arr !== value) {
+      return false;
+    }
+  }
+  // Change code above this line
+}
+
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus');
